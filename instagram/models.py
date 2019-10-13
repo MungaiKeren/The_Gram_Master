fa1c9212d2bc57bfaf18f934d0374d8c254f864d
MungaiKeren <wambukeren@gmail.com>
2019-10-13 22:35:05 +0300

    This commit displays the current logged in user and displays their profile picture and bio

--- instagram/models.py
+++ instagram/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = models.ImageField(upload_to='profile_photo/', blank=True)
-    bio = models.TextField(max_length=50, blank=True)
+    bio = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return f'{self.user.username}'
