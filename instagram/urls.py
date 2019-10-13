587254ee14f4b3d6cff3a1af1d3e1a8a71144461
MungaiKeren <wambukeren@gmail.com>
2019-10-13 20:52:53 +0300

    This commit creates a profile route

--- instagram/urls.py
+++ instagram/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url('register/', views.registration, name='registration')
+    url('register/', views.registration, name='registration'),
+    url('profile/', views.profile, name='profile')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
